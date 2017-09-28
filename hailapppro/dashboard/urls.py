17b917a61dbff1710df067b95f378aa12b5873da
doggra <Doggra>
2017-09-28 21:01:17 +0200

    set main views

--- hailapppro/dashboard/urls.py
+++ hailapppro/dashboard/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-from .views import IndexView
-
+from .views import MapView, SendClaimView, OpenClaimsView, \
+                   CompletedClaimsView, AdjustersView
 
 urlpatterns = [
-    url(r'$', IndexView.as_view(), name='index')
+    url(r'^$', MapView.as_view(), name='map'),
+    url(r'^send/$', SendClaimView.as_view(), name='send_claim'),
+    url(r'^open/$', OpenClaimsView.as_view(), name='open_claims'),
+    url(r'^completed/$', CompletedClaimsView.as_view(), name='completed_claims'),
+    url(r'^adjusters/$', AdjustersView.as_view(), name='adjusters'),
 ]
--- hailapppro/dashboard/views.py
+++ hailapppro/dashboard/views.py
@@ -5,5 +5,21 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 
-class IndexView(TemplateView):
+class MapView(TemplateView):
     template_name = 'dashboard/index.html'
+
+
+class SendClaimView(TemplateView):
+    template_name = 'dashboard/send_claim.html'
+
+
+class OpenClaimsView(TemplateView):
+    template_name = 'dashboard/open_claims.html'
+
+
+class CompletedClaimsView(TemplateView):
+    template_name = 'dashboard/completed_claims.html'
+
+
+class AdjustersView(TemplateView):
+    template_name = 'dashboard/adjusters.html'
