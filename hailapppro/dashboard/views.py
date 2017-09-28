740d259c6dc4db0589fe2fa7d2144f1b8bdff1f8
doggra <Doggra>
2017-09-28 21:55:31 +0200

    set full height, redraw map on resize

--- hailapppro/dashboard/views.py
+++ hailapppro/dashboard/views.py
@@ -6,7 +6,7 @@ from django.views.generic.base import TemplateView
 
 
 class MapView(TemplateView):
-    template_name = 'dashboard/index.html'
+    template_name = 'dashboard/map.html'
 
 
 class SendClaimView(TemplateView):
